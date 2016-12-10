df1cb44d857f7748574f668ef4400ec2e1407dec
Ryan Carlton <rlc120304@gmail.com>
2016-12-10 10:18:05 -0500

    Built a fully responsive table for the store...Added Sample animation to the landing page for later reference. Hours To Date 61

--- src/app/views/landing-page/landing-page.component.ts
+++ src/app/views/landing-page/landing-page.component.ts
@@ -1,11 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding,
+  trigger, transition, animate,
+  style, state } from '@angular/core';
+
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: 'landing-page.component.html',
-  styleUrls: ['landing-page.component.scss']
+  styleUrls: ['landing-page.component.scss'],
+  animations: [
+    trigger('routeAnimation', [
+      state('*',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)'
+        })
+      ),
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100%)'
+        }),
+        animate('0.2s ease-in')
+      ]),
+      transition(':leave', [
+        animate('0.5s ease-out', style({
+          opacity: 0,
+          transform: 'translateY(100%)'
+        }))
+      ])
+    ])
+  ]
 })
 export class LandingPageComponent implements OnInit {
+  @HostBinding('@routeAnimation') get routeAnimation() {
+    return true;
+  }
+
+  @HostBinding('style.display') get display() {
+    return 'block';
+  }
 
   constructor() { }
 
--- src/app/views/order-page/order-page.component.ts
+++ src/app/views/order-page/order-page.component.ts
@@ -1,9 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {ProductsService} from '../../services/products.service';
-import {OrdersService} from '../../services/orders.service';
-import {Product} from '../../models/product-model/product.model';
-import {Order} from '../../models/order-model/order.model';
 
 @Component({
   selector: 'app-order-page',
@@ -11,43 +6,10 @@ import {Order} from '../../models/order-model/order.model';
   styleUrls: ['order-page.component.css']
 })
 export class OrderPageComponent implements OnInit {
-  pagetitle: String = 'Order Page';
-  productsOrdered: Order[] = [];
 
-  constructor(private ordersService: OrdersService,
-              private router: Router) {
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.ordersService.get().then((orders) => {
-      this.productsOrdered = orders || [];
-    });
-  }
-
-  onOrder(): void {
-    debugger;
-    for(let i=0; i < this.productsOrdered.length; i++) {
-      this.ordersService.update(this.productsOrdered[i]);
-    }
-    this.router.navigate(['order']);
-  }
-
-  onProductSelected(product: Product) {
-    // TODO: Check that the product is not already added.
-    product.number = 1; // Make only one added now
-    let newOrder = new Order(product);
-    this.ordersService.create(newOrder).then((order) => {
-      this.productsOrdered.push(order);
-    });
-  }
-
-  deleteSelectProduct(order: Order) {
-    this.ordersService.deleteOrder(order).then((order: Order)=>{
-      let id = order.Id;
-      this.productsOrdered = this.productsOrdered.filter(order => order.Id !== id);
-      debugger;
-    });
-
   }
 
 }
